4472c477ad0f3ee6e2b3a92783a007b79fa4112c
5910406086 <46910948+5910406086@users.noreply.github.com>
2019-02-09 16:45:06 +0700

    Add files via upload

---
+++ JQuery/myscript.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {
+ 
+    $.getJSON("data.json", function(data){
+        var employee_data = '<tbody id="myTable">';
+        $.each(data, function(key, value){
+            employee_data += '<tr>';
+            employee_data += '<td>' + value.userId + '</td>';
+            employee_data += '<td>' + value.jobTitleName + '</td>';
+            employee_data += '<td>' + value.firstName + '</td>';
+            employee_data += '<td>' + value.lastName + '</td>';
+            employee_data += '<td>' + value.preferredFullName + '</td>';
+            employee_data += '<td>' + value.employeeCode + '</td>';
+            employee_data += '<td>' + value.region + '</td>';
+            employee_data += '<td>' + value.phoneNumber + '</td>';
+            employee_data += '<td>' + value.emailAddress + '</td>';
+            employee_data += '</tr>';
+        });
+        employee_data += '</tbody>';
+        $('#employee_table').append(employee_data);
+    });
+
+    $("#btn").click( function() {
+    var value = $("#myInput").val().toLowerCase();
+    $("#myTable tr").filter(function() {
+    $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+    });
+  });
+
+    
+
+});
+
+
+
